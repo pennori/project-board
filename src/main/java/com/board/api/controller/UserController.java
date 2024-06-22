150c8d0ba332fd2ef3068d9f685a5f781afb323b
pennori <pennorix@gmail.com>
2024-06-22 19:54:14 +0900

    Feat: 회원가입 ResponseEntity 적용

--- src/main/java/com/board/api/controller/UserController.java
+++ src/main/java/com/board/api/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +22,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public SignUpResponse doSignUp(@Valid @RequestBody SignUpRequest request) throws Exception {
+    public ResponseEntity<SignUpResponse> doSignUp(@Valid @RequestBody SignUpRequest request) throws Exception {
         SignUpResponse response = new SignUpResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST.name());
         long userSeq = userService.saveUser(request);
         if(0L != userSeq) {
             response = new SignUpResponse(String.valueOf(HttpStatus.OK.value()), HttpStatus.OK.name());
         }
-        log.info("response : {}", response);
-        return response;
+
+        return ResponseEntity.ok().body(response);
     }
 
 }
