11dd8614e05d10c7828dbef8e3270fada5bc10a5
pennori <pennorix@gmail.com>
2024-06-26 20:37:10 +0900

    Feat: 유효성 검증 정규표현식 수정

--- src/main/java/com/board/api/domain/member/dto/request/LoginRequest.java
+++ src/main/java/com/board/api/domain/member/dto/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.board.api.domain.member.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -13,10 +14,11 @@ import lombok.ToString;
 @NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "email is a required input.")
-    @Size(min = 1, max = 255, message = "userId size is between 1 and 255")
+    @Email(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "invalid email")
+    @Size(min = 1, max = 255, message = "email size is between 1 and 255")
     private String email;
 
     @NotBlank(message = "password is a required input.")
-    @Size(min = 1, max = 255, message = "password size is between 1 and 255")
+    @Size(min = 8, max = 16, message = "password size is between 8 and 16")
     private String password;
 }
--- src/main/java/com/board/api/domain/member/dto/request/SignUpRequest.java
+++ src/main/java/com/board/api/domain/member/dto/request/SignUpRequest.java
@@ -1,6 +1,8 @@
 package com.board.api.domain.member.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,18 +15,19 @@ import lombok.ToString;
 @NoArgsConstructor
 public class SignUpRequest {
     @NotBlank(message = "email is a required input.")
-    @Size(min = 1, max = 255, message = "userId size is between 1 and 255")
+    @Email(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "invalid email")
+    @Size(min = 1, max = 255, message = "email size is between 1 and 255")
     private String email;
 
     @NotBlank(message = "password is a required input.")
-    @Size(min = 1, max = 255, message = "password size is between 1 and 255")
+    @Size(min = 8, max = 16, message = "password size is between 8 and 16")
     private String password;
 
     @NotBlank(message = "name is a required input.")
-    @Size(min = 1, max = 255, message = "name size is between 1 and 255")
+    @Size(min = 1, max = 10, message = "name size is between 4 and 10")
     private String name;
 
     @NotBlank(message = "regNo is a required input.")
-    @Size(min = 1, max = 255, message = "idValue size is between 1 and 255")
+    @Pattern(regexp = "^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-8][0-9]{6}$", message = "invalid regNo")
     private String regNo;
 }
