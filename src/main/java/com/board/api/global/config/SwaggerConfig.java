cf1d7b85c6dd44e9ff74816b76328f8c7c589989
pennori <pennorix@gmail.com>
2025-02-12 23:23:11 +0900

    Refactor: SwaggerConfig

--- src/main/java/com/board/api/global/config/SwaggerConfig.java
+++ src/main/java/com/board/api/global/config/SwaggerConfig.java
@@ -12,31 +12,43 @@ import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
+        ApiMetadata apiMetadata = new ApiMetadata();
+        SecurityConfig securityConfig = new SecurityConfig();
+
         return new OpenAPI()
-                .info(getApiInfo())
-                .components(new Components().addSecuritySchemes("bearerAuth", getSecurityScheme()))
-                .security(Collections.singletonList(getSecurityRequirement()));
+                .info(apiMetadata.createApiInfo())
+                .components(new Components().addSecuritySchemes(SecurityConfig.BEARER_AUTH, securityConfig.createSecurityScheme()))
+                .security(Collections.singletonList(securityConfig.createSecurityRequirement()));
     }
 
-    private SecurityRequirement getSecurityRequirement() {
-        return new SecurityRequirement().addList("bearerAuth");
+    private static class ApiMetadata {
+        public Info createApiInfo() {
+            return new Info()
+                    .title("게시판 이용자의 포인트 관리 서비스 (토이 프로젝트)")
+                    .description("Java 17 & Spring Boot 3.3.1")
+                    .version("1.0.0");
+        }
     }
 
-    private SecurityScheme getSecurityScheme() {
-        return new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
-    }
+    private static class SecurityConfig {
+        public static final String BEARER_AUTH = "bearerAuth";
+        public static final String AUTH_HEADER_NAME = "Authorization";
+        public static final String BEARER_FORMAT = "JWT";
 
-    private Info getApiInfo() {
-        return new Info()
-                .title("게시판 이용자의 포인트 관리 서비스 (토이 프로젝트)")
-                .description("Java 17 & Spring Boot 3.3.1")
-                .version("1.0.0");
+        public SecurityRequirement createSecurityRequirement() {
+            return new SecurityRequirement().addList(BEARER_AUTH);
+        }
+
+        public SecurityScheme createSecurityScheme() {
+            return new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat(BEARER_FORMAT)
+                    .in(SecurityScheme.In.HEADER)
+                    .name(AUTH_HEADER_NAME);
+        }
     }
 }
